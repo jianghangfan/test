d61e4c31f23cc79d1664424bf5ccfa7c3f3105a9
jianghangfan <jianghf0527@163.com>
2020-06-09 12:34:16 +0800

    Spring搭建eureka 将项目改成maven父子项目

--- eureka/src/main/java/com/panda/EurekaApplication.java
+++ eureka/src/main/java/com/panda/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.panda.panda;
+package com.panda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PandaApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PandaApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
