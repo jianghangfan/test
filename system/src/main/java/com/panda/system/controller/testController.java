deb5679467dd9e7189793fcd0fc310a1cddc62af
jianghangfan <jianghf0527@163.com>
2020-06-09 18:32:33 +0800

    在gateway增加路由转发配置，调试成功

--- system/src/main/java/com/panda/system/controller/testController.java
+++ system/src/main/java/com/panda/system/controller/testController.java
@@ -2,7 +2,6 @@ package com.panda.system.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class testController {
 
