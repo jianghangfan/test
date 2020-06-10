0858543cd3bf3fcec1ac0494f28db8a92c5ca752
jianghangfan <jianghf0527@163.com>
2020-06-10 15:56:30 +0800

    增加server公共子模块

---
+++ server/src/main/java/com/panda/server/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.panda.server.mapper;
+
+import com.panda.server.dao.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+
+    public List<Test> testList();
+}
---
+++ server/src/main/java/com/panda/server/service/TestMapperImpl.java
@@ -0,0 +1,19 @@
+package com.panda.server.service;
+
+import com.panda.server.dao.Test;
+import com.panda.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestMapperImpl{
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> testList(){
+        return testMapper.testList();
+    }
+}
